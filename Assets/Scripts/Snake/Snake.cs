8870d646ebf9a6af71526ba20de09338e03f59e3
Edineuda Teixeira <edineuda@gmail.com>
2022-08-21 10:25:59 -0300

    Programação do movimento da snake

--- Assets/Scripts/Snake/Snake.cs
+++ Assets/Scripts/Snake/Snake.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Food : MonoBehaviour
+public class Snake : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 10);
+        
     }
 
     // Update is called once per frame
