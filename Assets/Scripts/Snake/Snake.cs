3ba0978b5edf8667b3039eccd053bfc1d0dc3c86
Edineuda Teixeira <edineuda@gmail.com>
2022-08-21 10:37:26 -0300

    Teste de movimentação da snake

--- Assets/Scripts/Snake/Snake.cs
+++ Assets/Scripts/Snake/Snake.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Direção que a cobra vai se movimentar
+    Vector2 dir = Vector2.right;
     void Start()
     {
-        
+        InvokeRepeating("Move", 0.3f, 0.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Move()
+    {
+        transform.Translate(dir);
     }
 }
